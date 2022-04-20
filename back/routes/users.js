c4a3c1a71542d562e4e6c62966d90112f00456cb
oliviacowan <olivia.cowan@hotmail.com>
2022-04-20 16:03:52 -0700

    fix users route

--- back/routes/users.js
+++ back/routes/users.js
@@ -3,7 +3,14 @@ const router = require('express').Router();
 module.exports = (db) => {
   router.get('/users', (req, res) => {
     db.query(
-      `SELECT * FROM users`
-    ).then(() => {res.json(res)})
-  })
+      `SELECT * FROM users;`
+    )
+      .then((response) => 
+
+      {  res.json(response.rows) })
+      .catch((err) => { console.log('There has been an ERROR: ', err) });
+  
+  });
+    return router;
 }
+
--- back/server.js
+++ back/server.js
@@ -6,22 +6,25 @@ const app = Express();
 const axios = require('axios');
 
 const BodyParser = require('body-parser');
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8089;
+
+const http = require('http');
+const server = http.createServer(app);
 
 const { Pool, Query } = require("pg");
 const dbParams = require("./lib/db");
 const db = new Pool(dbParams);
-db.connect();
+db.connect()
+.then(console.log('connected to db'))
+.catch((err) => console.log(err));
+
+const userRoute = require('./routes/users');
 
 app.use(BodyParser.urlencoded({ extended: false }));
 app.use(BodyParser.json());
 app.use(Express.static('public'));
 
-const http = require('http');
-const server = http.createServer(app);
-
-const usersRoute = require('./routes/users');
-app.use('/users', usersRoute(db));
+app.use('/', userRoute(db));
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)
